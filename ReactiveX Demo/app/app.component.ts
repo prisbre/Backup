9244c069236c3c441b4df2986e7450d9e82e54d0
prisbre <huanfeng.c@gmail.com>
2016-10-04 09:17:03 +0800

    update: Rxjs Demo app best practice

--- ReactiveX Demo/app/app.component.ts
+++ ReactiveX Demo/app/app.component.ts
@@ -1,22 +1,27 @@
 /// <reference path="../typings/tsd.d.ts" />
 
 import {Component} from 'angular2/core';
-// limit version of Observable by Angular team
+// strip down version of  Observable by Angular team
 // import {Observable} from 'rxjs/Observable';
 // import 'rxjs/add/operator/debounceTime';
 // import 'rxjs/add/operator/filter';
 import {Observable} from 'rxjs/Rx';
-
+// for best practice
+import {FormBuilder, ControlGroup} from 'angular2/common';
 
 @Component({
     selector: 'my-app',
     template: `
-        <input id="search" type="text" class="form-control">
+        <form [ngFormModel]="form">
+            <input id="search" type="text" class="form-control"
+                ngControl="search">
+            <!-- <div class="btn btn-primary" (click)="showDates()">button</div> -->
+        </form>
     `
 })
 export class AppComponent {
-    constructor(){
-        // the Callback Hell version
+    // constructor(){
+        // Callback Hell version
         // var debounced = _.debounce(function(text) {
         //     var url = 'https://api.spotify.com/v1/search?type=artist&q=' + text;
         //     $.getJSON(url, function (artists) {
@@ -34,27 +39,123 @@ export class AppComponent {
         // });
 
         // Observable version
+        // the following code Far from reach the BEST practice 
+        // as manipulate DOM in component
         
+        // let keyups = Observable.fromEvent($('#search'), 'keyup')
+        //     .map(e => e.target.value)
+        //     .filter(text => text.length >=3)
+        //     .debounceTime(400)
+        //     .distinctUntilChanged()
+        //     // logic in flatMap can wrap in service as API call, 
+        //     // like spotifyService.searchArtists
+        //     // here just expose to illustrate Rxjs usage
+        //     .flatMap(searchTerm => {
+        //         let url = 'https://api.spotify.com/v1/search?type=artist&q=' + searchTerm;
+        //         let promise = $.getJSON(url);
+        //         return Observable.fromPromise(promise);
+        //     });
+        // keyups.subscribe(data => console.log(data));      
+    // }
 
-        let keyups = Observable.fromEvent($('#search'), 'keyup')
-            .map(e => e.target.value)
+    // Angular2 best practice version 
+    form: ControlGroup;
+    constructor(fb: FormBuilder) {
+        this.form = fb.group({
+            search: []
+        })
+
+        let search = this.form.find('search');
+        search.valueChanges
             .filter(text => text.length >=3)
             .debounceTime(400)
             .distinctUntilChanged()
-            // logic in flatMap can wrap in service as API call, 
-            // like spotifyService.searchArtists
-            // here just expose to illustrate Rxjs usage
             .flatMap(searchTerm => {
                 let url = 'https://api.spotify.com/v1/search?type=artist&q=' + searchTerm;
                 let promise = $.getJSON(url);
                 return Observable.fromPromise(promise);
-            });
-
-        keyups.subscribe(data => console.log(data));
-        
-        console.log(new Observable());
-        
-        
+            })
+            .subscribe(x => console.log(x));
     }
+    
+
+
+
+
+    // a few Observable feature test:
+    // startDates = [];
+    // startDate = new Date();
+    
+    // showDates() {
+        // let day: number;
+        // for (day = 1; day <= 2; day++) {
+        //     var date = new Date(
+        //         this.startDate.getFullYear(),
+        //         this.startDate.getMonth(),
+        //         this.startDate.getDate() + day
+        //     );
+        //     this.startDates.push(date);
+        // };
+
+        // let a = Observable
+        //     .fromArray(this.startDates)
+        //     .map(date => {
+        //         console.log("Getting deals for date" + date);
+        //         return [1, 2, 3];
+        //     });
+            
+    
+        // Observable.of(['a', 'b'])
+        //     .subscribe(x => console.log(x));
+
+        // a.subscribe(x => console.log(x));
+
+
+        // Observable.interval(1000)
+        //     .subscribe(x => console.log(x));
+
+        // let b = Observable.throw(new Error('fasdf'));
+        // b.subscribe(
+        //     x => console.log(x), 
+        //     error => console.log(error)
+        // );
+
+        // b.retry(2);
+        // let count = 0;
+        // let ajax = Observable.of('url')
+        //     .flatMap(() => {
+        //         if (++count < 2) {
+        //             return Observable.throw(new Error('Request later'));
+        //         }
+        //         return Observable.of('afds');
+
+        //     });
+        // ajax.retry(3)
+        //     .subscribe(
+        //     x => console.log(x),
+        //     error => console.log(error)
+        // )
+
+        // let remoteDateStream = Observable.throw(
+        //     new Error('Something failed')
+        // );
+
+        // let remoteDateStream = Observable.of('ewrfd');
+        // remoteDateStream
+        //     .catch(error => {
+        //         let localDateStream = Observable.of('34wrf');
+        //         return localDateStream;
+        //     })
+        //     .subscribe(
+        //         x => console.log(x),
+        //         error => console.log(error),
+        //         () => console.log('completed'));
+    // };
+
+
 }
+
+
+
+
 
